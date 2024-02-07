f601fa9c6c79e738d2f32769131f859f94d3bd2f
hanskihamk03 <hanna23043@student.hamk.fi>
2024-02-07 20:01:45 +0200

    muutos

--- pages/connect.php
+++ pages/connect.php
@@ -4,7 +4,6 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $init=parse_ini_file("./.ht.asetukset.ini");
 
 try{
-    //$yhteys=mysqli_connect("db", "root", "password", "personnel");
     $yhteys=mysqli_connect($init["palvelin"],$init["tunnus"], $init["pass"], $init["tk"]);
 }
 catch(Exception $e){
