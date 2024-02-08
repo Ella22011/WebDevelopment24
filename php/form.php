e454f9ba331b4a101841013e181d6b3a635dfb4d
hanskihamk03 <hanna23043@student.hamk.fi>
2024-02-08 12:16:19 +0200

    muutos

--- php/form.php
+++ php/form.php
@@ -1,5 +1,9 @@
 <?php
 include ("./connect.php");
 
-$
+$ID=isset($_POST["ID"]) ? $_POST["ID"] : 0;
+$fName=isset($_POST["fName"]) ? $_POST["fName"] :"";
+$lName=isset($_POST["lName"]) ? $_POST["lName"] :"";
+$email=isset($_POST["email"]) ? $_POST["email"] :"";
+
 ?>
