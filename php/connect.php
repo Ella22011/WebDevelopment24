7ded010b9e8bc1fc990931f5cb0c6ee25dcbe227
hanskihamk03 <hanna23043@student.hamk.fi>
2024-02-14 10:07:35 +0200

    muutos

--- php/connect.php
+++ php/connect.php
@@ -6,7 +6,7 @@ $init = parse_ini_file("../pages/.ht.asetukset.ini");
 $yhteys = mysqli_connect($init["palvelin"], $init["tunnus"], $init["pass"], $init["tk"]);
 
 if (mysqli_connect_error()) {
-    header("Location: ./yhteysvirhe.html");
+    header("Location: ../pages/yhteysvirhe.html");
     exit;
 }
 ?>
