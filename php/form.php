8a5b086cdc5b098fba977156c0820b976e5e85b9
jemlau <142881394+jemlau@users.noreply.github.com>
2024-02-15 02:12:36 +0200

    Update form.php

--- php/form.php
+++ php/form.php
@@ -1,87 +1,30 @@
 <?php
-include("./connect.php");
+include ("./connect.php");
 ?>
 
 <?php
-if (isset($_POST["fName"])){
-    $fName=$_POST["fName"];
-}
-else{
-    $fName="";
-    header("Location:donate.html");
+
+$fName=isset($_POST["fName"]) ? $_POST["fName"] :"";
+$lName=isset($_POST["lName"]) ? $_POST["lName"] :"";
+$email=isset($_POST["email"]) ? $_POST["email"] :"";
+$donation=isset($_POST["donation"]) ? $_POST["donation"] : 0;
+$paymentMethod=isset($_POST["paymentMethod"]) ? $_POST["paymentMethod"] :"";
+$donationDate=isset($_POST["donationDate"]) ? $_POST["donationDate"] :"";
+
+if (empty($fName) || empty($lName) || empty($email) || empty($donation) || empty($paymentMethod) || empty($donationDate)){
+    header("Location:../pages/donate.html");
     exit;
 }
 
-if (isset($_POST["lName"])){
-    $lName=$_POST["lName"];
-}
-else{
-    $lName= "";
-    header("Location:donate.html");
-    exit;
-}
+$sql="insert into kissalahjoitukset (fName, lName, email, donation, paymentMethod, donationDate) values (?, ?, ?, ?, ?, ?)";
 
-if (isset($_POST["email"])){
-    $email=$_POST["email"];
-}
-else{
-    $email= "";
-    header("Location:donate.html");
-    exit;
-}
-
-if (isset($_POST["donation"])){
-    $donation=$_POST["donation"];
-}
-else{
-    $donation= "";
-    header("Location:donate.html");
-    exit;
-}
-
-if (isset($_POST["paymentMethod"])){
-    $paymentMethod=$_POST["paymentMethod"];
-}
-else{
-    $paymentMethod= "";
-    header("Location:donate.html");
-    exit;
-}
-
-if (isset($_POST["donationDate"])){
-    $donationDate=$_POST["donationDate"];
-}
-else{
-    $donationDate= "";
-    header("Location:donate.html");
-    exit;
-}
-
-$yhteys = mysqli_connect("localhost", "trtkp23_15", "nKudfqeT");
-
-if (!$yhteys){
-    die("Connection Error: " . mysqli_connect_error());
-}
-
-$tietokanta=mysqli_select_db($yhteys,"lahjoitustietokanta");
-if (!$tietokanta){
-    die("Error selecting Database: " . mysqli_connect_error());
-}
-echo"Database OK";
-
-$sql="insert into nimet(fName, lName, email) values(?,?,?)";
-$sql="insert into lahjoitukset(donation, paymentMethod, donationDate) values(?,?,?)";
 $stmt=mysqli_prepare($yhteys, $sql);
-mysqli_stmt_bind_param($stmt, 'sss', $fName, $lName, $email);
-mysqli_stmt_bind_param($stmt, 'dsd', $donation, $paymentMethod, $donationDate);
+
+mysqli_stmt_bind_param($stmt, 'sssisd', $fName, $lName, $email, $donation, $paymentMethod, $donationDate);
+
 mysqli_stmt_execute($stmt);
 
-mysqli_stmt_close($stmt);
 mysqli_close($yhteys);
 
 
-
-
-
 ?>
-
