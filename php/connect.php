b889d69381e064428fb0dd522b5397e88a95317b
jemlau <142881394+jemlau@users.noreply.github.com>
2024-02-15 03:42:38 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -4,10 +4,11 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $init = parse_ini_file("./.ht.asetukset.ini");
 
 try{
-    $yhteys=mysqli_connect($init["palvelin"], $init["tunnus"], $init["pass"], $init["tk"]);
+    $yhteys=mysqli_connect("localhost", "trtkp23_15", "nKudfqeT", "web_trtkp23_15");
 }
 catch(Exception $e){
     header("Location:../pages/yhteysvirhe.html");
     exit;
 }
+
 ?>
