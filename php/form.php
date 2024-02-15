eed658e698de8f205d5f67b4d73235ffbfc39072
jemlau <142881394+jemlau@users.noreply.github.com>
2024-02-15 03:42:36 +0200

    Update form.php

--- php/form.php
+++ php/form.php
@@ -1,15 +1,13 @@
 <?php
 include ("./connect.php");
-?>
-
-<?php
 
 $fName=isset($_POST["fName"]) ? $_POST["fName"] :"";
 $lName=isset($_POST["lName"]) ? $_POST["lName"] :"";
 $email=isset($_POST["email"]) ? $_POST["email"] :"";
 $donation=isset($_POST["donation"]) ? $_POST["donation"] : 0;
 $paymentMethod=isset($_POST["paymentMethod"]) ? $_POST["paymentMethod"] :"";
-$donationDate=isset($_POST["donationDate"]) ? $_POST["donationDate"] :"";
+$donationDate = isset($_POST["donationDate"]) ? date('Y-m-d', strtotime($_POST["donationDate"])) : "";
+
 
 if (empty($fName) || empty($lName) || empty($email) || empty($donation) || empty($paymentMethod) || empty($donationDate)){
     header("Location:../pages/donate.html");
@@ -20,11 +18,13 @@ $sql="insert into kissalahjoitukset (fName, lName, email, donation, paymentMetho
 
 $stmt=mysqli_prepare($yhteys, $sql);
 
-mysqli_stmt_bind_param($stmt, 'sssisd', $fName, $lName, $email, $donation, $paymentMethod, $donationDate);
+mysqli_stmt_bind_param($stmt, 'sssiss', $fName, $lName, $email, $donation, $paymentMethod, $donationDate);
 
 mysqli_stmt_execute($stmt);
 
 mysqli_close($yhteys);
 
+header("Location:../pages/thankyou.html");
 
+exit;
 ?>
