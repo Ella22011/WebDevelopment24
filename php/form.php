9eab6ed9df33f8e078ea0877be522b56c6c272f3
jemlau <142881394+jemlau@users.noreply.github.com>
2024-02-20 12:12:46 +0200

    Update form.php

--- php/form.php
+++ php/form.php
@@ -1,24 +1,23 @@
 <?php
 include ("./connect.php");
 
-$fName=isset($_POST["fName"]) ? $_POST["fName"] :"";
-$lName=isset($_POST["lName"]) ? $_POST["lName"] :"";
-$email=isset($_POST["email"]) ? $_POST["email"] :"";
-$donation=isset($_POST["donation"]) ? $_POST["donation"] : 0;
+$user_id=isset($_POST["user_id"]) ? $_POST["user_id"] :"0";
+$username=isset($_POST["username"]) ? $_POST["username"] :"";
+$donation_id=isset($_POST["donation_id"]) ? $_POST["donation_id"] : "0";
 $paymentMethod=isset($_POST["paymentMethod"]) ? $_POST["paymentMethod"] :"";
 $donationDate = isset($_POST["donationDate"]) ? date('Y-m-d', strtotime($_POST["donationDate"])) : "";
 
 
-if (empty($fName) || empty($lName) || empty($email) || empty($donation) || empty($paymentMethod) || empty($donationDate)){
+if (empty($user_id) || empty($username) || empty($donation_id) || empty($paymentMethod) || empty($donationDate)){
     header("Location:../pages/donate.html");
     exit;
 }
 
-$sql="insert into kissalahjoitukset (fName, lName, email, donation, paymentMethod, donationDate) values (?, ?, ?, ?, ?, ?)";
+$sql="insert into kissalahjoitukset (user_id, username, donation_id, paymentMethod, donationDate) values (?, ?, ?, ?, ?)";
 
 $stmt=mysqli_prepare($yhteys, $sql);
 
-mysqli_stmt_bind_param($stmt, 'sssiss', $fName, $lName, $email, $donation, $paymentMethod, $donationDate);
+mysqli_stmt_bind_param($stmt, 'isiss', $user_id, $username, $donation_id, $paymentMethod, $donationDate);
 
 mysqli_stmt_execute($stmt);
 
