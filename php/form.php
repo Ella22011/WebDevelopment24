4ea413e529430d23f4fc65b0fbf26af39873842d
hanskihamk03 <hanna23043@student.hamk.fi>
2024-02-20 14:57:45 +0200

    fooormi

--- php/form.php
+++ php/form.php
@@ -8,11 +8,10 @@ $paymentMethod=isset($_POST["paymentMethod"]) ? $_POST["paymentMethod"] :"";
 $donationDate = isset($_POST["donationDate"]) ? date('Y-m-d', strtotime($_POST["donationDate"])) : "";
 
 
-//if (empty($user_id) || empty($username) || empty($donation_id) || empty($paymentMethod) || empty($donationDate)){
-  //  header("Location:../pages/donate.html");
-   // exit;
-//}
-
+if (empty($user_id) || empty($username) || empty($donation_id) || empty($paymentMethod) || empty($donationDate)){
+    header("Location:../pages/donate.html");
+    exit;
+}
 $sql="INSERT INTO kissalahjoitukset (user_id, username, donation_id, paymentMethod, donationDate) values (?, ?, ?, ?, ?)";
 
 $stmt=mysqli_prepare($yhteys, $sql);
