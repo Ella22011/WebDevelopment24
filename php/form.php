089e808a00c9e927d6af07ce58758ae3240d537b
hanskihamk03 <hanna23043@student.hamk.fi>
2024-02-21 20:39:38 +0200

    donate

--- php/form.php
+++ php/form.php
@@ -9,15 +9,15 @@ $paymentMethod=isset($_POST["paymentMethod"]) ? $_POST["paymentMethod"] :"";
 $donationDate = isset($_POST["donationDate"]) ? date('Y-m-d', strtotime($_POST["donationDate"])) : "";
 
 
-if (empty($user_id) || empty($username) || empty($donation_id) || empty($paymentMethod) || empty($donationDate)){
+if (empty($user_id) || empty($username) || empty($donation_id) || empty($donation_amount) || empty($paymentMethod) || empty($donationDate)){
     header("Location:../pages/donate.html");
     exit;
 }
-$sql="INSERT INTO kissalahjoitukset (user_id, username, donation_id, paymentMethod, donationDate) values (?, ?, ?, ?, ?)";
+$sql="INSERT INTO kissalahjoitukset (user_id, username, donation_id, donation_amount, paymentMethod, donationDate) values (?, ?, ?, ?, ?, ?)";
 
 $stmt=mysqli_prepare($yhteys, $sql);
 
-mysqli_stmt_bind_param($stmt, 'isiiss', $user_id, $username, $donation_id, $paymentMethod, $donationDate);
+mysqli_stmt_bind_param($stmt, 'isisss', $user_id, $username, $donation_id, $donation_amount, $paymentMethod, $donationDate);
 
 mysqli_stmt_execute($stmt);
 
