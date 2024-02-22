5570a05dbd601c7cb7e9d692f472e0d22014c563
hanskihamk03 <hanna23043@student.hamk.fi>
2024-02-23 01:29:21 +0200

    muistiinpanoi

--- php/printprofile.php
+++ php/printprofile.php
@@ -1,16 +1,16 @@
 <?php
-// Tarkista, onko käyttäjä kirjautunut sisään
+// Tarkistaa, onko käyttäjä kirjautunut sisään
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    // Hae käyttäjän tiedot tietokannasta
-    // Voit käyttää samaa tietokantayhteyttä connect.php-tiedostosta
-    $stmt = $yhteys->prepare("SELECT fName, lName, email, username FROM users WHERE user_id = ?");
-    $stmt->bind_param("i", $_SESSION['user_id']);
+    
+    $stmt = $yhteys->prepare("SELECT fName, lName, email, username FROM users WHERE user_id = ?");// Hakee käyttäjän tiedot tietokannasta
+    $stmt->bind_param("i", $_SESSION['user_id']); //bind_param() -metodi määrittelee, mitkä arvot korvaavat valmistellun lauseen parametrit.
+                                                  //i tarkoittaa, että arvo on kokonaisluku (integer)
     $stmt->execute();
     $result = $stmt->get_result();
     $user = $result->fetch_assoc();
 
     
-    echo '<p>First Name: ' . $user['fName'] . '</p>';
+    echo '<p>First Name: ' . $user['fName'] . '</p>'; //
     echo '<p>Last Name: ' . $user['lName'] . '</p>';
     echo '<p>Email: ' . $user['email'] . '</p>';
     echo '<p>Username: ' . $user['username'] . '</p>';
