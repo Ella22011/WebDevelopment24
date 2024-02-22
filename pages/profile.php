2b95dae5ef900bbc55bf7251bc6e03e43c8112cd
hanskihamk03 <hanna23043@student.hamk.fi>
2024-02-22 11:50:53 +0200

    profile

--- pages/profile.php
+++ pages/profile.php
@@ -11,7 +11,7 @@ $donate_page = isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true ? 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Cat Distribution System, your local cat adoption shelter.">
     <link rel="icon" type="image/x-icon" href="../images/favicon-32x32.png">
-    <title>Cat Distribution System - About Us</title>
+    <title>Cat Distribution System - Profile</title>
     <link rel="stylesheet" href="../css/styles-ella.css">
 </head>
 
@@ -49,33 +49,10 @@ $donate_page = isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true ? 
 
     <main class="homepage">
         <article id="C1">
-            <h1>About us</h1>
-            <p>We're dedicated to ensuring that every cat finds a loving and caring home. We've witnessed heartwarming
-                instances where stray cats seem to choose or adopt their human companions. In these cases, a cat,
-                whether lost or seeking a new home, finds its way into the hearts of individuals willing to provide
-                care. Veterinary websites offer guidance on what to do when an unknown feline enters your life. Our
-                focus is on creating a platform where cats in need can find their way into loving homes. It's not just
-                about distribution; it's about fostering meaningful connections between cats and the families ready to
-                welcome them with open arms.</p>
+            <h1>Profile</h1>
+            
         </article>
         <img src="../images/websiteDividerPic.png" class="websiteDivider" alt="Pawprints">
-        <article id="C2">
-            <h1>Contact us</h1>
-            <p>
-                Got questions, concerns, or just want to talk cats? We're all ears! Reach out to us using the contact
-                information below:
-            </p>
-            <h3>Contact Information</h3>
-            <p>Email: info@catdistribution.com</p>
-            <p>Phone: 123-456-7890</p>
-
-            <h3>Visit Our Office</h3>
-            <p>Address: 123 Cat Street, Kittyville, CA 12345</p>
-
-            <h3>Business Hours</h3>
-            <p>Monday - Friday: 9:00 AM - 5:00 PM</p>
-            <p>Saturday - Sunday: Closed</p>
-        </article>
     </main>
 
     <footer class="footer">
