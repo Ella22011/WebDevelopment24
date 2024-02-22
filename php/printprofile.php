0f040e688fe51c8fd7345134c35ee9c2c5f212d5
hanskihamk03 <hanna23043@student.hamk.fi>
2024-02-23 01:58:00 +0200

    kommenttei

--- php/printprofile.php
+++ php/printprofile.php
@@ -1,15 +1,15 @@
 <?php
-// Tarkistaa, onko käyttäjä kirjautunut sisään
-if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { // Tarkistaa, onko käyttäjä kirjautunut sisään
     
     $stmt = $yhteys->prepare("SELECT fName, lName, email, username FROM users WHERE user_id = ?");// Hakee käyttäjän tiedot tietokannasta
     $stmt->bind_param("i", $_SESSION['user_id']); //bind_param() -metodi määrittelee, mitkä arvot korvaavat valmistellun lauseen parametrit.
                                                   //i tarkoittaa, että arvo on kokonaisluku (integer)
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $user = $result->fetch_assoc();
+    $stmt->execute(); //suorittaa yllä olevan valmistellun kyselyn
+    $result = $stmt->get_result(); // hakee kyselyn TULOKSET taulukosta (eli kaikki tiedot tietyltä user_id)
+    $user = $result->fetch_assoc(); //tässä vielä noudetaan KÄYTTÄJÄN tiedot
 
-    
+    //lopuksi tulostetaan noudetut tiedot
     echo '<p>First Name: ' . $user['fName'] . '</p>'; //
     echo '<p>Last Name: ' . $user['lName'] . '</p>';
     echo '<p>Email: ' . $user['email'] . '</p>';
